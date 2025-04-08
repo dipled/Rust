b2d07b5ade7158cdaba90819eaaad75738ae4137
helena <helenavargastannuri@gmail.com>
2025-04-08 13:32:16 -0300

    showdebola

--- Lista01/src/ex05.rs
+++ Lista01/src/ex05.rs
@@ -3,13 +3,7 @@ pub fn is_palindrome(s: &String) -> bool {
     let mut iter = s.chars();
     let mut iter2 = s.chars().rev();
     for _ in 0..size / 2 {
-        if let Some(c1) = iter.next() {
-            if let Some(c2) = iter2.next() {
-                if c1 != c2 {
-                    return false;
-                }
-            }
-        }
+        if iter.next() != iter2.next() {return false}
     }
     true
 }
